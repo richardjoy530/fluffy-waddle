10b9d1f446d78400467786a2343602e4c0592dce
Amaldev M V <amaldev.ad71868@gmail.com>
2026-07-25 12:52:28 +0530

    (feat): Header UI

--- app/src/app/layout/header/header.ts
+++ app/src/app/layout/header/header.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
-
+  protected readonly navLinks = [
+    { path: '/features', label: 'Features' },
+    { path: '/solutions', label: 'Solutions' },
+    { path: '/pricing', label: 'Pricing' },
+    { path: '/docs', label: 'Docs' },
+    { path: '/about', label: 'About' },
+  ];
 }
